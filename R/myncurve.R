db9362cf3f0b7a64258e1e1fd0c6bbb75fb76e5a
Emily Spotts <emily.spotts@ou.edu>
2024-03-01 22:25:06 -0600

    myncurve

--- R/myncurve.R
+++ R/myncurve.R
@@ -2,13 +2,28 @@
 #'
 #' @param mu
 #' @param sigma
+#' @param a
 #'
 #' @return curve and list
 #' @export
 #'
 #' @examples
 #' myncurve(mu=10,sigma=2)
-myncurve = function(mu, sigma){
+myncurve = function(mu, sigma, a){
   curve(dnorm(x,mean=mu,sd=sigma), xlim = c(mu-3*sigma, mu + 3*sigma))
-  list(mu = mu, sigma = sigma)
+  xcurve=seq(-Inf,a,length=1000)
+
+  # Y values corresponding t0 the x values
+  ycurve=dnorm(xcurve,mu,sigma)
+
+  # Fill in the polygon with the given vertices
+  polygon(c(-Inf,xcurve,a),c(0,ycurve,0),col="Red")
+
+  # Put in the text with the appropriate area
+
+  # Area
+  prob=pnorm(a, mu, sigma)
+  prob=round(prob,4)
+
+  list(mu = mu, sigma = sigma, prob=prob)
 }
