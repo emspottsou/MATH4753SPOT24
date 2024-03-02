735010fc97beb523f840d992ad32412830e17f57
Emily Spotts <emily.spotts@ou.edu>
2024-03-01 22:28:26 -0600

    myncurve

--- R/myncurve.R
+++ R/myncurve.R
@@ -11,13 +11,13 @@
 #' myncurve(mu=10,sigma=2)
 myncurve = function(mu, sigma, a){
   curve(dnorm(x,mean=mu,sd=sigma), xlim = c(mu-3*sigma, mu + 3*sigma))
-  xcurve=seq(6*sigma,a,length=1000)
+  xcurve=seq(-6*sigma,a,length=1000)
 
   # Y values corresponding t0 the x values
   ycurve=dnorm(xcurve,mu,sigma)
 
   # Fill in the polygon with the given vertices
-  polygon(c(6*sigma,xcurve,a),c(0,ycurve,0),col="Red")
+  polygon(c(-6*sigma,xcurve,a),c(0,ycurve,0),col="Red")
 
   # Put in the text with the appropriate area
 
