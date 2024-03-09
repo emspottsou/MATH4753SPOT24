bddb8b1a33df227696d5f3fdb33642d4fb1c3c6d
Emily Spotts <emily.spotts@ou.edu>
2024-03-08 23:51:38 -0600

    data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' FIREDAM.csv
+#'
+#' Damages caused by fires I think
+#'
+#' @format ## `fire`
+#' A data frame with 15 rows and 2 columns:
+#' \describe{
+#'   \item{DISTANCE}{Distance}
+#'   \item{DAMAGE}{Damage}
+#'   ...
+#' }
+#' @source <FIREDAM.csv>
+"fire"
