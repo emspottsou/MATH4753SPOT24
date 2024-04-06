00200481e8b098b7d410af9c31c0fff7145bb041
Emily Spotts <emily.spotts@ou.edu>
2024-04-06 16:13:12 -0500

    ntickets

---
+++ R/ntickets.R
@@ -0,0 +1,34 @@
+#' ntickets
+#'
+#' @param N numeric vector
+#' @param gamma numeric vector
+#' @param p numeric vector
+#'
+#' @return list
+#' @export
+#'
+#' @examples ntickets(N=400, gamma=0.02, p=0.95)
+ntickets=function(N, gamma, p){
+  # Using the discrete binomial distribution:
+  binomfind <- function(N, gamma, p, nd){
+    pbinom(q=N, size=nd, prob=p, lower.tail=FALSE)
+  }
+  nd = N # not possible to overbook if less tickets sold than seats
+  while(binomfind(N=N, gamma=gamma, p=p, nd=nd+1) - gamma < 0){ # check if next value of n is too large
+    nd = nd + 1 # if n is not too large, n is increased and checked again
+  }
+
+  plot(N:(nd+20), binomfind(N=N, gamma=gamma, p=p, nd=N:(nd+20)), type="b", xlab="n", ylab="Objective", pch=19, main="Objective function to find n using binomial distribution")
+  abline(h=0, v=nd)
+
+  # Using the continuous normal approximation:
+  approxnorm=function(n){
+    pnorm(q=N+0.5, mean=p*n, sd=sqrt(n*p*(1-p)), lower.tail=FALSE) - gamma # probability of overbooking minus gamma
+  }
+  nc <- uniroot(approxnorm, interval=c(N, N+20))$root # value for which probability of overbooking is equal to gamma
+
+  curve(approxnorm(x), from=N, to=(nc+20), xlab="n", ylab="Objective", main="Objective function to find n with normal approximation")
+  abline(h=0,v=nc)
+
+  list(nd=nd, nc=nc, N=N, gamma=gamma, p=p)
+}
